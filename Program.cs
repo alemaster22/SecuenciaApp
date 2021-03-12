e618cc49e7a7f31b896c6b6414e35e0297ab4fbf
alemaster22 <alemaster222@gmail.com>
2021-03-11 23:24:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SecuenciaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Secuencia App \n El siguiente programa permite la generación de una secuencia númerica dados dos valores enteros \n, uno de inicio y uno de fin ");
+
+            GeneradorSeq();
+
+            static void GeneradorSeq()
+            {
+                int num1, num2;
+                
+                try
+                {
+                    num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Por favor digite un número para especificar el final de la secuencia seguido de la tecla intro: ");
+                    num2 = int.Parse(Console.ReadLine());
+
+                    if (num2 < num1)
+                    {
+                        Console.WriteLine("el número seleccionado como frontera superior de la secuencia debe ser mayor al número del comienzo de la secuencia ");
+                        return;
+                    }
+
+                    for (int i=num1;i<=num2;i++) {
+                        Console.WriteLine(i);
+                    }
+
+                }
+                catch (FormatException eF)
+                {
+                    Console.WriteLine("La aplicación solo admite valores númericos");
+                }
+                catch (OverflowException oE)
+                {
+                    Console.WriteLine("Se ha introducido un valor muy alto , intente nuevamente");
+                }
+            }
+
+        }
+    }
+}
